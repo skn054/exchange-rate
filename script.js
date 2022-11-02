1d8e959ece4196c58de5f66d0b50256d452ff1be
Bhanu Kiran <Bhanu.thathari96@gmail.com>
2022-11-02 13:07:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,31 @@ const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
+function caclulate(){
+    const rate_one = amountEl_one.value;
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    fetch(`https://open.exchangerate-api.com/v6/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+        rateEl.innerText = `1 ${currency_one} = ${data.rates[currency_two]} ${currency_two}` ;
+        amountEl_two.value = rate_one * data.rates[currency_two];
+    })
+}
 
+function swapFucntion(){
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    caclulate();
+}
 
 // Event listeners
 currencyEl_one.addEventListener('change', caclulate);
 amountEl_one.addEventListener('input', caclulate);
 currencyEl_two.addEventListener('change', caclulate);
-amountEl_two.addEventListener('input', caclulate);
+swap.addEventListener('click',swapFucntion)
+// amountEl_two.addEventListener('input', caclulate);
 
 
 
